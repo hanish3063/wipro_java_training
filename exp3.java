547aef92aef0bb23407a6eb3c5a995f8783463ff
HANISH.E <90960034+hanish3063@users.noreply.github.com>
2023-02-06 19:24:16 -0900

    Create exp3.java

---
+++ exp3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class LargestOfTwoNumbers {
+	public static void main(String[] args) {
+		
+		Scanner s1 = new Scanner(System.in);
+		System.out.print("Input the First Number:");
+		int n1 = s1.nextInt();
+		System.out.print("Input the Second Number:");
+		int n2 = s1.nextInt();
+		
+		if (n1==n2)
+		{
+			System.out.print("Both are equal");
+		}
+		
+		if (n1>n2)
+		{
+			System.out.print(n1 + " " + "is the greatest number");
+		}
+		
+		if (n2>n1) {
+			System.out.print(n2 +" " +  "is the greatest number");
+		}
+		
+	}
+	
+	
+
+}
