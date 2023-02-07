93b6794916343b784a160c28f4097bdd3a3dc8cf
HANISH.E <90960034+hanish3063@users.noreply.github.com>
2023-02-06 19:25:28 -0900

    Create exp4.java

---
+++ exp4.java
@@ -0,0 +1,16 @@
+public class PrintEvenNumbersZeroToHundred {
+    
+    public static void main(String args[])   
+{  
+int number=100;  
+System.out.print("List of even numbers from 0 to 100:");  
+for (int i=0; i<=number; i++)   
+{  
+if (i%2==0)   
+{  
+System.out.print(i + " ");  
+}  
+}  
+}  
+    
+}
