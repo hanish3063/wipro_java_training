1a8384b64442a45b96872981a540e0def658b215
HANISH.E <90960034+hanish3063@users.noreply.github.com>
2023-02-06 19:33:52 -0900

    Create exp5.java

---
+++ exp5.java
@@ -0,0 +1,13 @@
+public class PrintNumbersBackwards {
+    
+    public static void main(String args[]){
+    
+        int number=100;
+        System.out.println("The numbers from 1 to 100 in reverse order : ");
+        for (int i=number; i>=1; i--){
+        
+            System.out.println(i + " ");
+        }
+    }
+    
+}
