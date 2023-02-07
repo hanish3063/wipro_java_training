5bdf40aa2caa1447d8429ab7864ff1ddcadcabfa
HANISH.E <90960034+hanish3063@users.noreply.github.com>
2023-02-06 19:35:00 -0900

    Create exp6.java

---
+++ exp6.java
@@ -0,0 +1,11 @@
+public class PrintNumbersOneToHundred {
+    public static void main(String args[]) {
+    System.out.println("Numbers from One to 100:");
+    for (int i=1; i<=100; i++) {
+    
+      System.out.println(i + ",");
+    }
+    }
+    
+    
+}
